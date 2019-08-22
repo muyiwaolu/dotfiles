e61064a54a8907c6a61d6c6324a662c4d3e2d43c
Muyiwa Olu <muyiwaolu@users.noreply.github.com>
2019-08-22 16:28:11 +0100

    cljfmt actually works

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.21.1"]
-                  [lein-cljfmt "0.6.4"]
-                  [jonase/eastwood "0.3.5"]]}}
+{:user {:plugins  [[cider/cider-nrepl "0.21.1"]
+                   [lein-cljfmt "0.6.4"]
+                   [jonase/eastwood "0.3.5"]]
+        :dependencies
+        [[cljfmt "0.5.1"]]}}
